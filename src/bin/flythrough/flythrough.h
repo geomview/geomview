70933067f1ec4cf543aa1a47885922edde094011
Claus-Justus Heine <rotdrop@geomview>
2006-07-30 03:55:28 +0000

    Xforms issue.

--- src/bin/flythrough/flythrough.h
+++ src/bin/flythrough/flythrough.h
@@ -1,3 +1,8 @@
+#ifndef GV_FLYTHROUGH_H
+#define GV_FLYTHROUGH_H
+
+#include <forms.h>
+
 #define LOOP 	0
 #define QUARTER	1
 #define DIRECT	2
@@ -6,3 +11,4 @@
 #define EUC 0
 #define HYP 1
 
+#endif
