96a1ac312711a99c940d154a295582adef5f7078
Claus-Justus Heine <rotdrop@geomview>
2007-02-14 15:12:56 +0000

    --print-BLAH switches to figure out the location of the data directory etc. of an already installed geomview.

--- geomview.sh
+++ geomview.sh
@@ -2,7 +2,6 @@
 
 # geomview shell script driver
 
-GEOMROOT=%GEOMROOT%
 if test -x %GEOMVIEW_LTGVX%; then
     GEOMVIEW_GVX=%GEOMVIEW_LTGVX%
 else
@@ -25,7 +24,6 @@ export LD_LIBRARY_PATH
 ### setting them in your shell environment before invoking this
 ### script.
 ### 
-### GEOMVIEW_DIR: pathname of geomview distribution directory
 ### GEOMVIEW_GVX: pathname of executable file "gvx"
 ### GEOMVIEW_LOAD_PATH: colon-separated list of directories to search
 ### 	for files
@@ -46,7 +44,7 @@ suf=
 : ${GEOMVIEW_EMODULE_PATH=%MODULESDIR%}
 : ${GEOMVIEW_SYSTEM_INITFILE=${GEOMDATA}/.geomview}
 
-export GEOMROOT GEOMVIEW_GVX GEOMVIEW_LOAD_PATH GEOMDATA \
+export GEOMVIEW_GVX GEOMVIEW_LOAD_PATH GEOMDATA \
         GEOMVIEW_EMODULE_PATH GEOMVIEW_SYSTEM_INITFILE
 
 if [ -d /cygdrive ]; then
@@ -58,73 +56,8 @@ fi
 gvx="${GEOMVIEW_GVX}"
 case "$gvx" in
   *gvx.[a-zA-Z3])
-    gvx="`expr match \"$GEOMVIEW_GVX\" '(.*)\.[^./]*' \| \"$GEOMVIEW_GVX\"`" ;;
+    gvx="`expr match \"${GEOMVIEW_GVX}\" '(.*)\.[^./]*' \| \"${GEOMVIEW_GVX}\"`" ;;
 esac
-
-can_C3D() {
-  case "$DISPLAY" in
-    :*|unix:*) test -w /dev/fbs/ffb0 || return 1 ;;
-  esac
-  case `ldd "${gvx}.C3D" 2>&1` in
-    *found*|*"No such"*) return 1 ;;
-  esac
-}
-
-gvx_option() {
- case "$1" in
-  -ogl|-OGL|-opengl|-OpenGL|-OPENGL)
-	suf=.OGL; suf2=.MESA
-	if [ "$MACHTYPE" = "solaris" ]; then
-	    suf=.MESA
-	    if can_C3D; then suf=.C3D; fi
-	fi
-	shift ;;
-  -mesa|-Mesa|-MESA)
-	suf=.MESA; suf2=.OGL ;;
-  -x11|-X11)
-	suf=.X11; shift ;;
-  -c3d|-C3D|-Creator3D|-creator3d)
-	suf=.C3D; suf2=.MESA ;;
-  -h|-help|-HELP|-\?*)
-    echo "For advice on selecting which geomview graphics option to use,
-see also \"geomview -graphics\"."
-    exit 0
-    ;;
-  -grap*|-Grap*|-GRAP*)
-    if [ -f "$gvx.X11" -o -f "$gvx.MESA" -o -f "$gvx.C3D" -o -f "$gvx.OGL" ]; then
-	g=`basename $0`
-	echo "The $0 script can invoke any of several executables:"
-	test -f "$gvx.X11" && \
-	  echo "  with geomview's own X software rendering: use $g -x11"
-	test -f "$gvx.MESA" && \
-	  echo "  with Mesa OpenGL-like software rendering: use $g -mesa"
-	test -f "$gvx.X11" && \
-	  echo "  with Sun OpenGL for UltraSparcs with Creator3D: use $g -creator3d"
-	test -f "$gvx.OGL" && \
-	  echo "  with Open GL: use $g -opengl"
-	echo "If given, this option MUST BE THE FIRST on geomview's command line."
-	echo "Using some variant of Open GL allows transparency and texture-mapping;"
-	echo "software OpenGL implementations are slower than $g -x11, but hardware ones"
-	echo "run faster."
-	echo "To change the default, setenv GEOMVIEW_GVXOPT to the appropriate option."
-    else
-	echo "No alternative graphics options available."
-    fi >&2
-    exit 0
-    ;;
-  *) test ;;
- esac
-}
-
-gvx_option ${GEOMVIEW_GVXOPT}
-
-if gvx_option "$1"; then shift; fi
-
-
-if [ "$MACHTYPE" = "solaris" -a -z "$suf" -a -w /dev/fbs/ffb0 ] && can_C3D; then
-  echo "$0: Invoking Creator3D version (see \"geomview -graphics\" for info)" >&2
-  suf=.C3D
-fi
 
 if [ -f "$gvx$suf" ]; then
   GEOMVIEW_GVX="$gvx$suf"
@@ -136,10 +69,39 @@ fi
 export GEOMVIEW_GVX
 
 case "$1" in
-  -dbx) GEOMVIEW_DEBUG=1 exec dbx $GEOMVIEW_GVX ;;
-  -edge) GEOMVIEW_DEBUG=1 exec edge $GEOMVIEW_GVX ;;
-  -gdb) GEOMVIEW_DEBUG=1 exec gdb $GEOMVIEW_GVX ;;
-  -xdb) GEOMVIEW_DEBUG=1 exec xdb $GEOMVIEW_GVX ;;
-  -ddd) GEOMVIEW_DEBUG=1 exec ddd $GEOMVIEW_GVX ;;
-  *) exec $GEOMVIEW_GVX ${1+"$@"} ;;
+    -dbx) GEOMVIEW_DEBUG=1 exec dbx ${GEOMVIEW_GVX} ;;
+    -edge) GEOMVIEW_DEBUG=1 exec edge ${GEOMVIEW_GVX} ;;
+    -gdb) GEOMVIEW_DEBUG=1 exec gdb ${GEOMVIEW_GVX} ;;
+    -xdb) GEOMVIEW_DEBUG=1 exec xdb ${GEOMVIEW_GVX} ;;
+    -ddd) GEOMVIEW_DEBUG=1 exec ddd ${GEOMVIEW_GVX} ;;
+    -h|--help) cat <<EOF
+Script options:
+
+-dbx|edge|gdb|xdb|ddd Setup the environment and start the specfified debugger
+
+-h|--help Print this help
+
+--print-geomdata              Print the location of the main data directory
+--print-geomview-load-path    Print the data load-path
+--print-geomview-emodule-path Print the emodule search-path
+--print-geomview-system-initfile    Print the path to the system-wide init file
+--print-geomview-gvx          Print the path to the gvx executable
+--print-geomview-lib          Print the path to the geomview library
+--print-geomview-include      Print the path to the geomview include directory
+
+Options understood by the Geomview binary:
+
+EOF
+    exec ${GEOMVIEW_GVX} -h
+    ;;
+
+    --print-geomdata) echo ${GEOMDATA} ;;
+    --print-geomview-load-path) echo ${GEOMVIEW_LOAD_PATH} ;;
+    --print-geomview-emodule-path) echo ${GEOMVIEW_EMODULE_PATH} ;;
+    --print-geomview-system-initfile) echo ${GEOMVIEW_SYSTEM_INITFILE} ;;
+    --print-geomview-gvx) echo ${GEOMVIEW_GVX} ;;
+    --print-geomview-lib) echo %GEOMVIEW_LIB% ;;
+    --print-geomview-include) echo %GEOMVIEW_INCLUDE% ;;
+
+  *) exec ${GEOMVIEW_GVX} ${1+"$@"} ;;
 esac
