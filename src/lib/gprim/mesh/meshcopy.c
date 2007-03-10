279e37f700ba85d243c7c9ac46b791ded171731d
Claus-Justus Heine <rotdrop@geomview>
2007-03-10 12:51:58 +0000

    Ansification.

--- src/lib/gprim/mesh/meshcopy.c
+++ src/lib/gprim/mesh/meshcopy.c
@@ -34,54 +34,60 @@ Copyright (C) 1998-2000 Stuart Levy, Tamara Munzner, Mark Phillips";
 #include "meshP.h"
 
 Mesh *
-MeshCopy(obj)
-    Mesh *obj;
+MeshCopy(Mesh *obj)
 {
-	Mesh  *m;
-	Mesh  *om = obj;
-	int    n;
+    Mesh  *m;
+    Mesh  *om = obj;
+    int    n;
 
-	if (om == NULL) return (NULL);
+    if (om == NULL) return (NULL);
 
-	if ( (m = GeomNew(Mesh)) == NULL ) {
-           GeomError(0,"Can't allocate space for mesh");
-           return (NULL);
-        }
+    if ( (m = GeomNew(Mesh)) == NULL ) {
+	GeomError(0,"Can't allocate space for mesh");
+	return (NULL);
+    }
 
-	*m = *om; /* copy scalar fields */
-	n = m->nu * m->nv;
-	if ((m->p = GeomNewN(HPoint3, n)) == NULL) {
-           GeomError(0,"Can't allocate space for mesh vertices");
-           return (NULL);
-        }
-	memcpy(m->p, om->p, n * sizeof(HPoint3));
+    *m = *om; /* copy scalar fields */
+    n = m->nu * m->nv;
+    if ((m->p = GeomNewN(HPoint3, n)) == NULL) {
+	GeomError(0,"Can't allocate space for mesh vertices");
+	return (NULL);
+    }
+    memcpy(m->p, om->p, n * sizeof(HPoint3));
 
-	if (m->geomflags & MESH_N) {
-           if ((m->n = GeomNewN(Point3, n)) == NULL) {
-                GeomError(0,"Can't allocate space for mesh normals");
-                return(NULL);
-           }
-           memcpy(m->n, om->n, n * sizeof(Point3));
-	} else
-		m->n = NULL;
+    if (m->geomflags & MESH_N) {
+	if ((m->n = GeomNewN(Point3, n)) == NULL) {
+	    GeomError(0,"Can't allocate space for mesh normals");
+	    return(NULL);
+	}
+	memcpy(m->n, om->n, n * sizeof(Point3));
+    } else
+	m->n = NULL;
 
-	if (m->geomflags & MESH_C) {
-           if ((m->c = GeomNewN(ColorA, n)) == NULL) {
-                GeomError(0,"Can't allocate space for mesh colors");
-                return(NULL);
-           }
-	   memcpy(m->c, om->c, n * sizeof(ColorA));
-	} else
-		m->c = NULL;
+    if (m->geomflags & MESH_C) {
+	if ((m->c = GeomNewN(ColorA, n)) == NULL) {
+	    GeomError(0,"Can't allocate space for mesh colors");
+	    return(NULL);
+	}
+	memcpy(m->c, om->c, n * sizeof(ColorA));
+    } else
+	m->c = NULL;
 
-	if (m->geomflags & MESH_U) {
-           if ((m->u = GeomNewN(Point3, n)) == NULL) {
-                GeomError(0,"Can't allocate space for mesh texture");
-                return(NULL);
-           }
-	   memcpy(m->u, om->u, n * sizeof(Point3));
-	} else
-                m->u = NULL;
+    if (m->geomflags & MESH_U) {
+	if ((m->u = GeomNewN(Point3, n)) == NULL) {
+	    GeomError(0,"Can't allocate space for mesh texture");
+	    return(NULL);
+	}
+	memcpy(m->u, om->u, n * sizeof(Point3));
+    } else
+	m->u = NULL;
 
-	return m;
+    return m;
 }
+
+/*
+ * Local Variables: ***
+ * mode: c ***
+ * c-basic-offset: 4 ***
+ * End: ***
+ */
