32f78abc97c40066d52f10c1c4a73f39f474a8ec
Jorge Barros de Abreu <ficmatinfmag@geomview>
2008-08-24 09:53:24 +0000

    script for manual pt_BR creation only (adjust) script for diacritical marks in pt_BR

--- doc/pt_BR/geomview_cvs.sh
+++ doc/pt_BR/geomview_cvs.sh
@@ -1,5 +1,6 @@
 #commands for manual-pt_BR creation for the manual compilation only
 #texi2dvi needs two excutions for index and links creation
+#created on a slackware 12
 sh acentos_texinfo.sh ../geomview-pt_BR.texi
 texi2dvi --pdf --batch --lang=pt ../geomview-pt_BR.texi
 texi2dvi --pdf --batch --lang=pt ../geomview-pt_BR.texi
