7aa4ce8821c51c3b48461d991435b630c0b1f39e
Jorge Barros de Abreu <ficmatinfmag@geomview>
2008-08-24 09:44:59 +0000

    script for manual pt_BR creation only

---
+++ doc/pt_BR/geomview_cvs.sh
@@ -0,0 +1,8 @@
+#commands for manual-pt_BR creation for the manual compilation only
+#texi2dvi needs two excutions for index and links creation
+sh acentos_texinfo.sh ../geomview-pt_BR.texi
+texi2dvi --pdf --batch --lang=pt ../geomview-pt_BR.texi
+texi2dvi --pdf --batch --lang=pt ../geomview-pt_BR.texi
+makeinfo --force --enable-encoding ../geomview-pt_BR.texi -o geomview.info
+texi2html --lang=pt --split=chapter --output=tmp-html ../geomview-pt_BR.texi
+rm *.vr *.tp *.toc *.pg *.ky *BR.log *.fn* *.cp *.aux
