e1f6b98ca7fba847d8d871e0d126c5bc501677ba
Jorge Barros de Abreu <ficmatinfmag@geomview>
2010-07-25 10:28:40 +0000

    Manual Portuguese Translation. Updating locally compilation script.

--- doc/pt_BR/geomview_cvs.sh
+++ doc/pt_BR/geomview_cvs.sh
@@ -1,9 +1,9 @@
 #commands for manual-pt_BR creation for the manual compilation only
 #texi2dvi needs two excutions for index and links creation
-#created on a slackware 12
-sh acentos_texinfo.sh ../geomview-pt_BR.texi
-texi2dvi --pdf --batch --lang=pt ../geomview-pt_BR.texi
-texi2dvi --pdf --batch --lang=pt ../geomview-pt_BR.texi
+#created on a slackware 12 under GPL by
+#Jorge Barros de Abreu
+texi2dvi --pdf --batch ../geomview-pt_BR.texi
+texi2dvi --pdf --batch ../geomview-pt_BR.texi
 makeinfo --force --enable-encoding ../geomview-pt_BR.texi -o geomview.info
-texi2html --lang=pt --split=chapter --output=tmp-html ../geomview-pt_BR.texi
+texi2html --split=chapter --output=tmp-html ../geomview-pt_BR.texi
 rm *.vr *.tp *.toc *.pg *.ky *BR.log *.fn* *.cp *.aux
