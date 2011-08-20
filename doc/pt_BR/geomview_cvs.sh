f1028d82e896a790162928a5c65d607154e4fd00
Jorge Barros de Abreu <ficmatinfmag@geomview>
2011-08-20 22:54:41 +0000

    adjusting for convert /figs/*.ps to pdf.

--- doc/pt_BR/geomview_cvs.sh
+++ doc/pt_BR/geomview_cvs.sh
@@ -2,8 +2,14 @@
 #texi2dvi needs two excutions for index and links creation
 #created on a slackware 12 under GPL by
 #Jorge Barros de Abreu
+cd ../figs
+eps2pdf1.sh
+cd ../pt_BR
 texi2dvi --pdf --batch ../geomview-pt_BR.texi
 texi2dvi --pdf --batch ../geomview-pt_BR.texi
 makeinfo --force --enable-encoding ../geomview-pt_BR.texi -o geomview.info
 texi2html --split=chapter --output=tmp-html ../geomview-pt_BR.texi
 rm *.vr *.tp *.toc *.pg *.ky *BR.log *.fn* *.cp *.aux
+cd ../figs
+rm *.pdf
+cd ../pt_BR
