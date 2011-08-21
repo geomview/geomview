6f7e4f2cf92fbd0323982cb0f0f9580f0fa6a3df
Jorge Barros de Abreu <ficmatinfmag@geomview>
2011-08-21 09:06:01 +0000

    add the "pt" code language into script

--- doc/pt_BR/geomview_cvs.sh
+++ doc/pt_BR/geomview_cvs.sh
@@ -1,4 +1,4 @@
-#commands for manual-pt_BR creation for the manual compilation only
+#commands for manual-pt_BR creation (manual compilation only)
 #texi2dvi needs two excutions for index and links creation
 #created on a slackware 12 under GPL by
 #Jorge Barros de Abreu
@@ -8,7 +8,7 @@ cd ../pt_BR
 texi2dvi --pdf --batch ../geomview-pt_BR.texi
 texi2dvi --pdf --batch ../geomview-pt_BR.texi
 makeinfo --force --enable-encoding ../geomview-pt_BR.texi -o geomview.info
-texi2html --split=chapter --output=tmp-html ../geomview-pt_BR.texi
+texi2html -lang pt --split=chapter --output=tmp-html ../geomview-pt_BR.texi
 rm *.vr *.tp *.toc *.pg *.ky *BR.log *.fn* *.cp *.aux
 cd ../figs
 rm *.pdf
